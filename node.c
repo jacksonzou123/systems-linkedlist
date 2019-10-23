a9ce6c0c017d7ee84bb32b42542f5eb7c1a5f786
Jackson <jzou00@stuy.edu>
2019-10-23 11:10:44 -0400

    added files in

---
+++ node.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"node.h"
+
+void print_list(struct node *) {
+
+}
+
+struct node * insert_front(struct node *, int) {
+
+}
+
+struct node * free_list(struct node *) {
+
+}
---
+++ node.h
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+struct node {
+  int i;
+  struct node *next;
+};
+
+void print_list(struct node *);
+
+struct node * insert_front(struct node *, int);
+
+struct node * free_list(struct node *);
